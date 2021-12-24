07dcd44582b0b33157fdf2bd7a7f4dcb270288c4
marwana2001 <58035531+marwana2001@users.noreply.github.com>
2021-12-24 22:09:06 +0200

    Update problem_14.java

--- problem_14.java
+++ problem_14.java
@@ -1,42 +1,29 @@
-package prolem_2;
-import static java.nio.file.Files.size;
 import java.util.Hashtable;
 import java.util.Scanner;
-public class PairSum {
 
-
-
-
-
-public static void getPairs(int arr[], int target, int size){
-   Hashtable<Integer, Integer> hash = new Hashtable<Integer, Integer>();
-
-    for(int i=0;i<arr.length;i++){
-        int temp= target-arr[i];
-        if(hash.containsKey(arr[i])){
-
-            System.out.println("pair found");
-            System.out.println("[ " + arr[i] +" , "+ hash.get(arr[i])+ " ]");}
-            
-        else
-            
-            hash.put(temp, arr[i]);
+public class problem14 {
+    public static void getPairs(int arr[], int target) {
+        int state = 0;
+        Hashtable<Integer, Integer> Pairs = new Hashtable<Integer, Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            int temp = target - arr[i];
+            if (Pairs.containsKey(arr[i])) {
+                state++;
+                System.out.println("pair found");// hence we print all exist pairs because we don't sure doctor need
+                                                 // only one pair or all pairs
+                System.out.println("[ " + arr[i] + " , " + Pairs.get(arr[i]) + " ]");
+            } else
+                Pairs.put(temp, arr[i]);
+        }
+        if (state == 0)
+            System.out.println("pair not found");
     }
-
-}
-
-
-
-
-public static void main(String[] args) {        
-
-    Scanner input=new Scanner(System.in);
-    int arr[] = {2,45,7,3,5,1,8,9};
-    int size = arr.length;
-    int target=0;
-    System.out.println("enter target...");
-    target=input.nextInt();
-    getPairs(arr,target,size);
-    
-}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int arr[] = { 2, 45, 7, 3, 5, 8, 9 };
+        int target = 0;
+        System.out.println("enter target...");
+        target = input.nextInt();
+        getPairs(arr, target);
+    }
 }
